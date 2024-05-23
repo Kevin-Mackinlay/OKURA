239a654e5ec847dc24159cbbfbd302963cecba3c
Mackinlay Kevin <kemack83@gmail.com>
2024-05-23 17:33:31 -0300

    working on jwt

--- routes/auth.js
+++ routes/auth.js
@@ -1,6 +1,7 @@
 const router = require('express').Router();
 const User = require('../models/User');
 const CryptoJS = require('crypto-js');
+const jwt = require('jsonwebtoken');
 
 //register
 router.post('/register', async (req, res) => {
@@ -25,11 +26,16 @@ router.post('/login', async (req, res) => {
     !user && res.status(401).json('Wrong username!');
     const hashPassword = CryptoJS.AES.decrypt(user.password, process.env.PASS_SECRET);
 
-    const password = hashPassword.toString(CryptoJS.enc.Utf8);
+    const originalPassword = hashPassword.toString(CryptoJS.enc.Utf8);
 
-    password !== req.body.password && res.status(401).json('Wrong password!');
+    originalPassword !== req.body.password && 
+    res.status(401).json('Wrong password!');
 
-    res.status(200).json(user);
+    const accessToken = jwt.sign({ id: user._id, isAdmin: user.isAdmin }, process.env.JWT_SECRET, { expiresIn: '3d' });
+
+
+    const { password, ...others } = user._doc;
+    res.status(200).json({...others, accessToken});
   } catch (err) {
     res.status(500).json(err);
   }
