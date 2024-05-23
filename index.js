2c17e3a7caa7338b025bdbafa1a9978da86dd1be
Mackinlay Kevin <kemack83@gmail.com>
2024-05-23 12:54:31 -0300

    commit

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
+const { error } = require('console');
 const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
+
+mongoose.connect('mongodb+srv://ecommerce:3dTVWUMI2NgEwdzT@ecommerce.osbcjez.mongodb.net/').then(()=> 
+    console.log("Connected to database!")).catch((error) => console.log(error));
+
+
+
 
 
 app.listen(5000, () => {
     console.log("Backend server is running!");
 });
-
