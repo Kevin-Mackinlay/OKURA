ef14a7aa8695cbc2f7f623df8a30304fafc5d642
Mackinlay Kevin <kemack83@gmail.com>
2024-05-23 14:30:06 -0300

    avanzando en las rutas

--- index.js
+++ index.js
@@ -1,15 +1,24 @@
-const { error } = require('console');
 const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+const userRoute = require('./routes/user');
+const authRoute = require('./routes/auth');
 
-mongoose.connect('mongodb+srv://ecommerce:3dTVWUMI2NgEwdzT@ecommerce.osbcjez.mongodb.net/').then(()=> 
-    console.log("Connected to database!")).catch((error) => console.log(error));
+dotenv.config();
+
+mongoose
+  .connect(process.env.MONGO_URL)
+  .then(() => console.log('Connected to database!'))
+  .catch((error) => {
+    console.log(error);
+  });
+
+app.use(express.json());
+app.use('/api/auth', authRoute);
+app.use('/api/users', userRoute);
 
 
-
-
-
-app.listen(5000, () => {
-    console.log("Backend server is running!");
+app.listen(process.env.PORT || 5000, () => {
+  console.log('Backend server is running!');
 });
