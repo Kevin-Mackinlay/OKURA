e5b3bc81ee8b9e5f0d5df3353bc65f9be11bea60
Mackinlay Kevin <kemack83@gmail.com>
2024-05-23 16:47:05 -0300

    working on login

--- routes/auth.js
+++ routes/auth.js
@@ -1,17 +1,35 @@
 const router = require('express').Router();
 const User = require('../models/User');
+const CryptoJS = require('crypto-js');
 
 //register
 router.post('/register', async (req, res) => {
   const newUser = new User({
     username: req.body.username,
     email: req.body.email,
-    password: req.body.password,
+    password: CryptoJS.AES.encrypt(req.body.password, process.env.PASS_SECRET).toString(),
   });
 
   try {
     const savedUser = await newUser.save();
-   res.status(201).json(savedUser);
+    res.status(201).json(savedUser);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+//login
+router.post('/login', async (req, res) => {
+  try {
+    const user = await User.findOne({ username: req.body.username });
+    !user && res.status(401).json('Wrong username!');
+    const hashPassword = CryptoJS.AES.decrypt(user.password, process.env.PASS_SECRET);
+
+    const password = hashPassword.toString(CryptoJS.enc.Utf8);
+
+    password !== req.body.password && res.status(401).json('Wrong password!');
+
+    res.status(200).json(user);
   } catch (err) {
     res.status(500).json(err);
   }
