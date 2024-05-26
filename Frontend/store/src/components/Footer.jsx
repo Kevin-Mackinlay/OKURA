1c396db85b5596e0ed808b963c5e53c621cda9b6
Mackinlay Kevin <kemack83@gmail.com>
2024-05-26 18:38:45 -0300

    finshed productsList page

---
+++ Frontend/store/src/components/Footer.jsx
@@ -0,0 +1,127 @@
+import styled from 'styled-components';
+import { Email, Facebook, Instagram, Phone, Pinterest, Room, Twitter } from '@material-ui/icons';
+
+const Container = styled.div`
+  display: flex;
+  justify-content: space-between;
+`;
+
+const Left = styled.div`
+  flex: 1;
+  display: flex;
+  flex-direction: column;
+  padding: 20px;
+`;
+
+const Logo = styled.h1``;
+const Desc = styled.p`
+  margin: 20px 0;
+`;
+const SocialContainer = styled.div`
+  display: flex;
+`;
+const SocialIcon = styled.div`
+  width: 40px;
+  height: 40px;
+  border-radius: 50%;
+  color: white;
+  background-color: #${(props) => props.color};
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  margin-right: 20px;
+`;
+
+const Center = styled.div`
+  flex: 1;
+  padding: 20px;
+`;
+const Title = styled.h3`
+  margin-bottom: 30px;
+`;
+
+const List = styled.ul`
+  margin: 0;
+  padding: 0;
+  list-style: none;
+  display: flex;
+  flex-wrap: wrap;
+`;
+
+const ListItem = styled.li`
+  width: 50%;
+  margin-bottom: 10px;
+`;
+
+const Right = styled.div`
+  flex: 1;
+  padding: 20px;
+`;
+
+const ContactItem = styled.div`
+    margin-bottom: 20px;
+    display: flex;
+    align-items: center;
+`;
+
+const Payment = styled.img`
+    width: 50%;
+`;
+
+
+const Footer = () => {
+  return (
+    <Container>
+      <Left>
+        <Logo>OKANE</Logo>
+        <Desc>Okane is a stylish e-commerce platform that offers a curated selection of high-quality, trendy clothing. Committed to sustainable and ethical fashion, Okane provides a seamless shopping experience for fashion-forward individuals looking for style and substance.</Desc>
+        <SocialContainer>
+          <SocialIcon color="3B5999">
+            <Facebook />
+          </SocialIcon>
+          <SocialIcon color="E4405F">
+            <Instagram />
+          </SocialIcon>
+          <SocialIcon color="E60023">
+            <Pinterest />
+          </SocialIcon>
+          <SocialIcon color="55ACEE">
+            <Twitter />
+          </SocialIcon>
+        </SocialContainer>
+      </Left>
+      <Center>
+        <Title>Useful Links</Title>
+        <List>
+          <ListItem>Home</ListItem>
+          <ListItem>Cart</ListItem>
+          <ListItem>Man Clothes</ListItem>
+          <ListItem>Woman Clothes</ListItem>
+          <ListItem>Accessories</ListItem>
+          <ListItem>My Account</ListItem>
+          <ListItem>Order Tracking</ListItem>
+          <ListItem>Wishlist</ListItem>
+          <ListItem>Terms</ListItem>
+        </List>
+      </Center>
+      <Right>
+        <Title>Contact</Title>
+        <ContactItem>
+          <Room style={{ marginRight: '10PX' }} />
+          1629 jackson street, Hollywood Florida 33020
+        </ContactItem>
+        <ContactItem>
+          <Phone style={{ marginRight: '10PX' }} />
+          +1 954 512 5865
+        </ContactItem>
+        <ContactItem>
+          <Email style={{ marginRight: '10PX' }} />
+          kemack83@gmail.com
+        </ContactItem>
+        <Payment src="https://i.ibb.co/Qfvn4z6/payment.png" />
+      </Right>
+    </Container>
+  );
+};
+
+export default Footer;
---
+++ Frontend/store/src/pages/ProductList.jsx
@@ -0,0 +1,75 @@
+import styled from 'styled-components';
+import Navbar from '../components/Navbar';
+import Products from '../components/Products';
+import Footer from '../components/Footer';
+
+const Container = styled.div`
+
+`;
+
+const Title = styled.h1`
+  margin: 20px;
+`;
+const FilterContainer = styled.div`
+  display: flex;
+  justify-content: space-between;
+`;
+
+const Filter = styled.span`
+  margin: 20px;
+`;
+
+const FilterText = styled.span`
+  font-size: 20px;
+  font-weight: 600;
+    margin-right: 20px;
+`;
+
+const Select = styled.select`
+  padding: 10px;
+  margin-right: 20px;
+ 
+`;
+
+const Option = styled.option`
+  
+`;
+
+const ProductList = () => {
+  return (
+    <Container>
+      <Navbar />
+      <Title>Tshirts</Title>
+      <FilterContainer>
+        <Filter>
+          <FilterText>Filter Products:</FilterText>
+          <Select>
+            <Option disabled>Color</Option>
+            <Option>White</Option>
+            <Option>Black</Option>
+          </Select>
+            <Select>
+                <Option disabled>Size</Option>
+                <Option>XS</Option>
+                <Option>S</Option>
+                <Option>M</Option>
+                <Option>L</Option>
+                <Option>XL</Option>
+            </Select>
+        </Filter>
+        <Filter>
+          <FilterText>Sort Products:</FilterText>
+            <Select>
+                <Option disabled>Price</Option>
+                <Option>Low to High</Option>
+                <Option>High to Low</Option>
+            </Select>
+        </Filter>
+      </FilterContainer>
+      <Products />
+      <Footer />
+    </Container>
+  );
+};
+
+export default ProductList;
