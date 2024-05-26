086ae3e727393490bc175bb7adbb5061aaaf6705
Mackinlay Kevin <kemack83@gmail.com>
2024-05-25 21:11:01 -0300

    working slider

---
+++ Frontend/store/src/data.js
@@ -0,0 +1,23 @@
+export const sliderItems = [
+  {
+    id: 1,
+    img: 'https://www.bootbarn.com/dw/image/v2/BCCF_PRD/on/demandware.static/-/Sites-master-product-catalog-shp/default/dwb9466dae/images/955/1000020955_400_P1.JPG?sw=1980&sh=1980&sm=fit',
+    title: 'JEANS SALE',
+    desc: 'Best quality clothes.',
+    bg: 'f0f8ff',
+  },
+  {
+    id: 2,
+    img: 'https://basketworld.com/27967-large_default/nike-gt-cut-3-cool-grey.jpg',
+    title: 'SHOES SALE',
+    desc: 'Best quality clothes.',
+    bg: 'f0f8ff',
+  },
+  {
+    id: 3,
+    img: 'https://th.bing.com/th/id/R.4b823e2813fd567656e0fc8b333f9087?rik=dgBQeWgxY4eBXA&riu=http%3a%2f%2fwww.backcountry.com%2fimages%2fitems%2f900%2fREF%2fREF1545%2fBK.jpg&ehk=OXUHy%2fsaDI0Ev%2bcHp8kI31%2f6Re%2fI8bSZZmEnieZp6TE%3d&risl=&pid=ImgRaw&r=0',
+    title: 'HAT SALE',
+    desc: 'Best quality clothes.',
+    bg: 'f0f8ff',
+  },
+];
