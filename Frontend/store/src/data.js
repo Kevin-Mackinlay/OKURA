8929d3e5f14620f627fe0486f6c5aad063a52724
Mackinlay Kevin <kemack83@gmail.com>
2024-05-26 15:33:34 -0300

    working with categories

--- Frontend/store/src/data.js
+++ Frontend/store/src/data.js
@@ -16,8 +16,27 @@ export const sliderItems = [
   {
     id: 3,
     img: 'https://th.bing.com/th/id/R.4b823e2813fd567656e0fc8b333f9087?rik=dgBQeWgxY4eBXA&riu=http%3a%2f%2fwww.backcountry.com%2fimages%2fitems%2f900%2fREF%2fREF1545%2fBK.jpg&ehk=OXUHy%2fsaDI0Ev%2bcHp8kI31%2f6Re%2fI8bSZZmEnieZp6TE%3d&risl=&pid=ImgRaw&r=0',
-    title: 'HAT SALE',
+    title: 'HATS SALE',
     desc: 'Best quality clothes.',
     bg: 'f0f8ff',
   },
 ];
+
+export const categories = [
+  {
+    id: 1,
+    img: 'https://images.pexels.com/photos/24287035/pexels-photo-24287035/free-photo-of-mujer-edificio-pared-muro.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+    title: 'SHIRT STYLE',
+  },
+  {
+    id: 2,
+    img: 'https://images.pexels.com/photos/16094250/pexels-photo-16094250/free-photo-of-mano-sujetando-zapatillas-entrenadores.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+    title: 'SHOES',
+  },
+  {
+    id: 3,
+    img: 'https://images.pexels.com/photos/1878821/pexels-photo-1878821.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+    title: 'HATS',
+  },
+];
+
