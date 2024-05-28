214c8305cf0bff75fac6174554c5499cfd7f8222
Mackinlay Kevin <kemack83@gmail.com>
2024-05-28 18:42:41 -0300

    finishing login

--- Frontend/store/src/index.js
+++ Frontend/store/src/index.js
@@ -2,7 +2,8 @@ import React from 'react';
 import { createRoot } from 'react-dom/client'; // Correct import for React 18
 import App from './App.jsx';
 import { Provider } from 'react-redux';
-import store from './redux/store';
+import {store, persistor} from './redux/store';
+import {PersistGate} from 'redux-persist/integration/react';
 
 const container = document.getElementById('root'); // Get the container element
 const root = createRoot(container); // Create a root
@@ -10,6 +11,8 @@ const root = createRoot(container); // Create a root
 // Render the App component within the Provider
 root.render(
   <Provider store={store}>
+    <PersistGate loading={null} persistor={persistor}>
     <App />
+    </PersistGate>
   </Provider>
 );
--- Frontend/store/src/redux/store.js
+++ Frontend/store/src/redux/store.js
@@ -1,9 +1,34 @@
 import { configureStore } from '@reduxjs/toolkit';
 import cartReducer from './cartRedux';
+import userReducer from './userRedux';
+import { persistStore, persistReducer, FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
 
+// Configuration for persisting the user reducer
+const persistConfig = {
+  key: 'root',
+  version: 1,
+  storage,
+};
 
-export default configureStore({
+const persistedReducer = persistReducer(persistConfig, userReducer);
+
+// Create and configure the store
+const store = configureStore({
   reducer: {
     cart: cartReducer,
+    user: persistedReducer,
   },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
 });
+
+// Create the persistor
+const persistor = persistStore(store);
+
+// Export the store and persistor
+export { store, persistor };
