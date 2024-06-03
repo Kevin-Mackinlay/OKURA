c7de20bf8e926b5d1fd7b0a605661298c2b1c63a
Mackinlay Kevin <kemack83@gmail.com>
2024-06-03 19:43:04 -0300

    modificaciones

--- API/routes/user.js
+++ API/routes/user.js
@@ -1,6 +1,7 @@
 const router = require('express').Router();
 const { verifyToken, verifyTokenAndAuthorization, verifyTokenAndAdmin } = require('./verifyToken');
 const { updateUser, deleteUser, getUser, getAllUsers, getUserStats, uploadProfileImage } = require('../controllers/userController');
+const upload = require('../uploads/multer')
 
 router.put('/:id', verifyTokenAndAuthorization, updateUser);
 router.delete('/:id', verifyTokenAndAuthorization, deleteUser);
