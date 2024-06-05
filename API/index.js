2a57504da55c22849145f24eaf0303da6a7f3f06
Mackinlay Kevin <kemack83@gmail.com>
2024-06-04 22:47:10 -0300

    correccion en swagger

--- API/index.js
+++ API/index.js
@@ -1,7 +1,6 @@
 const dotenv = require('dotenv');
 dotenv.config();
 
-
 const fs = require('fs');
 const yaml = require('js-yaml');
 const path = require('path');
@@ -13,6 +12,13 @@ const config = yaml.load(configFile);
 
 const cors = require('cors');
 const express = require('express');
+const swaggerUi = require('swagger-ui-express');
+const YAML = require('yamljs');
+
+const userDocs = YAML.load('./docs/user.yml');
+const productDocs = YAML.load('./docs/products.yml');
+const cartDocs = YAML.load('./docs/cart.yml');
+
 const app = express();
 const mongoose = require('mongoose');
 const userRoute = require('./routes/user');
@@ -23,7 +29,6 @@ const orderRoute = require('./routes/order');
 const stripeRoute = require('./routes/stripe');
 
 // Connect to database
-
 mongoose
   .connect(process.env.MONGO_URL)
   .then(() => console.log('DB Connection Successfull!'))
@@ -34,6 +39,11 @@ mongoose
 app.use(express.json());
 app.use(cors());
 
+// Swagger setup
+app.use('/api-docs/users', swaggerUi.serve, swaggerUi.setup(userDocs));
+app.use('/api-docs/products', swaggerUi.serve, swaggerUi.setup(productDocs));
+app.use('/api-docs/carts', swaggerUi.serve, swaggerUi.setup(cartDocs));
+
 // Route middleware
 app.use('/api/auth', authRoute);
 app.use('/api/users', userRoute);
@@ -41,7 +51,6 @@ app.use('/api/products', productRoute);
 app.use('/api/carts', cartRoute);
 app.use('/api/orders', orderRoute);
 app.use('/api/checkout', stripeRoute);
-
 
 app.listen(process.env.PORT || 5000, () => {
   console.log('Backend server is running!');
